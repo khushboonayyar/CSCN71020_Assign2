fffc6d986e366b9cc90ee3e548de54bc7b484064
khushboonayyar <khushboonayyar7@gmail.com>
2025-10-19 18:26:48 -0400

    rectangletest project created

---
+++ rectangletest/rectangletest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace rectangletest
+{
+	TEST_CLASS(rectangletest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
