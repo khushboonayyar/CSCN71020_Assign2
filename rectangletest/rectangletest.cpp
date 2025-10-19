34342c28750846e23b09137d7ea77c815dc42a55
khushboonayyar <khushboonayyar7@gmail.com>
2025-10-19 18:51:42 -0400

    perimeter and area

--- rectangletest/rectangletest.cpp
+++ rectangletest/rectangletest.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" int getPerimeter(int*, int*);
+extern "C" int getArea(int*, int*);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace rectangletest
@@ -8,9 +11,29 @@ namespace rectangletest
 	TEST_CLASS(rectangletest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		//task1
+		TEST_METHOD(TestGetperimeter_ValidInputs)
 		{
+			int length = 10;
+			int width = 5;
+
+			int perimeter = getPerimeter(&length, &width);
+
+			Assert::AreEqual(30, perimeter);
 		}
+
+		TEST_METHOD(TestGetArea_ValidInputs)
+		{
+
+			int length = 8;
+			int width = 4;
+
+			int area = getArea(&length, &width);
+
+			Assert::AreEqual(32, area);
+
+		}
+
 	};
 }
+
